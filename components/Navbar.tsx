e60265064e3f2388b39e936ec85b811215726b17
luvgo <jh93817@gmail.com>
2023-02-04 00:51:01 -0600

    improved navbar and added it in _app.tsx, also added a row componenet for bug tracker, added prisma(PlanetScale Has been implemented, and added incremental to ts.config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,31 +2,11 @@ import Link from "next/link";
 import { VscAccount } from "react-icons/vsc";
 import { MdNotificationsNone, MdDarkMode } from "react-icons/md";
 
-export const SideBar = () => {
-  return (
-    <nav className="flex h-screen w-fit flex-col pl-14 text-left text-lg text-white">
-      <Link href="/user" className="py-1">
-        Dashboard
-      </Link>
-      <Link href="/user/bugtracker" className="py-1">
-        All Issues
-      </Link>
-      <Link href="" className="py-1">
-        My Issues
-      </Link>
-      <Link href="" className="py-1">
-        Submit Issue
-      </Link>
-    </nav>
-  );
-};
-
 const Navbar = () => {
   return (
     <>
-      <div className="pt-5" />
-      <header className="flex items-center justify-between text-white">
-        <h1 className="bold pl-14 text-2xl">Bug Tracker</h1>
+      <header className="flex w-screen items-center justify-between bg-slate-500 p-5 text-white">
+        <h1 className="bold pl-9 text-2xl">Bug Tracker</h1>
         <nav className="flex justify-end">
           <MdDarkMode className="mx-5" size="30" />
           <input
@@ -40,7 +20,6 @@ const Navbar = () => {
           </Link>
         </nav>
       </header>
-      <div className="py-2" />
     </>
   );
 };
--- src/pages/user/bugtracker.tsx
+++ src/pages/user/bugtracker.tsx
@@ -1,70 +1,89 @@
 import Head from "next/head";
 import Navbar from "../../../components/Navbar";
-import { SideBar } from "../../../components/Navbar";
-import { MdNotificationsNone } from "react-icons/md";
 
-export default function BugTracker() {
+type rowProps = {
+  title: string;
+  priority: string;
+  status: string;
+  assignee: string;
+  timeAdded: string;
+  comments: string;
+};
+
+const Row = (props: rowProps) => {
+  return (
+    <tr className="border-b bg-gray-100 hover:bg-orange-100">
+      <td className="p-3 px-5">
+        <input type="text" value={props.title} className="bg-transparent" />
+      </td>
+      <td className="p-3 px-5">
+        <span className="bg-transparent">{props.priority}</span>
+      </td>
+      <td className="p-3 px-5">
+        <span className="bg-transparent">{props.status}</span>
+      </td>
+      <td className="p-3 px-5">
+        <span className="bg-transparent">{props.assignee}</span>
+      </td>
+      <td className="p-3 px-5">
+        <span className="bg-transparent">{props.timeAdded}</span>
+      </td>
+      <td className="p-3 px-5">
+        <span className="bg-transparent">{props.comments}</span>
+      </td>
+      <td className="flex justify-end p-3 px-5">
+        <button
+          type="button"
+          className="focus:shadow-outline mr-3 rounded bg-blue-500 py-1 px-2 text-sm text-white hover:bg-blue-700 focus:outline-none"
+        >
+          Save
+        </button>
+        <button
+          type="button"
+          className="focus:shadow-outline mr-3 rounded bg-green-500 py-1 px-2 text-sm text-white hover:bg-green-700 focus:outline-none"
+        >
+          Modify
+        </button>
+        <button
+          type="button"
+          className="focus:shadow-outline rounded bg-red-500 py-1 px-2 text-sm text-white hover:bg-red-700 focus:outline-none"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+const BugTracker = () => {
   return (
     <>
       <Head>
         <title>bug-tracker</title>
         <meta name="description" content="bug-tracker" />
       </Head>
-      <main className="h-full w-full bg-slate-700 font-sans">
-        <Navbar />
-        <div className="flex text-gray-900">
-          <SideBar />
-          <div className="mx-auto p-4">
-            <table className="text-md min-w-screen mb-4 rounded bg-white shadow-md">
+      <main className="font-san h-screen w-screen bg-slate-700">
+        <div className="flex justify-center text-gray-900">
+          <div className="p-4">
+            <table className="text-md rounded bg-white shadow-md">
               <tbody>
-                <tr className="border-b">
-                  <th className="p-3 px-5 text-left">Name</th>
-                  <th className="p-3 px-5 text-left">Email</th>
-                  <th className="p-3 px-5 text-left">Role</th>
+                <tr className="h-fit border-b">
+                  <th className="p-3 px-5 text-left">Title</th>
+                  <th className="p-3 px-5 text-left">Priority</th>
+                  <th className="p-3 px-5 text-left">Status</th>
+                  <th className="p-3 px-5 text-left">Assignee</th>
+                  <th className="p-3 px-5 text-left">Time Added</th>
+                  <th className="p-3 px-5 text-left">Comments</th>
                   <th></th>
                 </tr>
-                <tr className="border-b bg-gray-100 hover:bg-orange-100">
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.name"
-                      className="bg-transparent"
-                    />
-                  </td>
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.email"
-                      className="bg-transparent"
-                    />
-                  </td>
-                  <td className="p-3 px-5">
-                    <select value="user.role" className="bg-transparent">
-                      <option value="user">user</option>
-                      <option value="admin">admin</option>
-                    </select>
-                  </td>
-                  <td className="flex justify-end p-3 px-5">
-                    <button
-                      type="button"
-                      className="focus:shadow-outline mr-3 rounded bg-blue-500 py-1 px-2 text-sm text-white hover:bg-blue-700 focus:outline-none"
-                    >
-                      Save
-                    </button>
-                    <button
-                      type="button"
-                      className="focus:shadow-outline mr-3 rounded bg-green-500 py-1 px-2 text-sm text-white hover:bg-green-700 focus:outline-none"
-                    >
-                      Modify
-                    </button>
-                    <button
-                      type="button"
-                      className="focus:shadow-outline rounded bg-red-500 py-1 px-2 text-sm text-white hover:bg-red-700 focus:outline-none"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
+                <Row
+                  title="This is a bug"
+                  priority="URGENT"
+                  status="OPEN"
+                  assignee="Juan"
+                  timeAdded="2-3-2023"
+                  comments="comments"
+                />
               </tbody>
             </table>
           </div>
@@ -72,4 +91,6 @@ export default function BugTracker() {
       </main>
     </>
   );
-}
+};
+
+export default BugTracker;
